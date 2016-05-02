206e0aa9ef567fc9630c6e1d1fa9c2587d386370
cle61 <prodhomme.clement@hotmail.fr>
2016-05-02 14:34:13 +0200

    ajout des liens restant pour le footer

--- db/schema.rb
+++ db/schema.rb
@@ -13,14 +13,16 @@
 
 ActiveRecord::Schema.define(version: 20160501113503) do
 
+  create_table "ar_internal_metadata", primary_key: "key", force: :cascade do |t|
+    t.string   "value",      limit: 255
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+  end
+
   create_table "categories", force: :cascade do |t|
-    t.string   "name",        limit: 255
-    t.text     "description", limit: 65535
-    t.integer  "parent_id",   limit: 4
-    t.string   "image",       limit: 255
-    t.datetime "created_at",                null: false
-    t.datetime "updated_at",                null: false
-    t.string   "slug",        limit: 255
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+    t.string   "slug",       limit: 255
   end
 
 end
