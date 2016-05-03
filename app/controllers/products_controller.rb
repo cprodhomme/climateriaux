3d566cbdc4759eac295fbd035b183aeeb62ed2f1
Cle61 <prodhomme.clement@hotmail.fr>
2016-05-04 00:21:13 +0200

    permet d'ajouter un item au panier et affiche un produit donnée via un slug

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,6 @@
 class ProductsController < ApplicationController
   def show
+    @product = Product.find(params[:slug])
+    @order_item = current_order.order_items.new
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160503123326) do
+ActiveRecord::Schema.define(version: 20160503221748) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name",        limit: 255
@@ -62,6 +62,7 @@ ActiveRecord::Schema.define(version: 20160503123326) do
     t.integer  "category_id", limit: 4
     t.datetime "created_at",                                         null: false
     t.datetime "updated_at",                                         null: false
+    t.string   "slug",        limit: 255
   end
 
   add_index "products", ["category_id"], name: "index_products_on_category_id", using: :btree
