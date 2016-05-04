a2d8c558ddb3ac28f2429fb226315e4bc7761e16
Cle61 <prodhomme.clement@hotmail.fr>
2016-05-04 20:27:09 +0200

    corrige bug sur le show de categorie

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -5,7 +5,8 @@ class CategoriesController < ApplicationController
 
   def show
     category = Category.where(slug: params[:slug])
-    @products = Product.where(category: category)
+    @products = Product.where(category_id: category)
+    @order_item = current_order.order_items.new
   end
 
 end
