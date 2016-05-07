f2bc08dabb2d69ed952c8169b7bbac22f51fcf2b
Cle61 <prodhomme.clement@hotmail.fr>
2016-05-07 16:31:27 +0200

    intégration des détails techniques sur la page produit

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,6 +2,7 @@ class ProductsController < ApplicationController
   def show
     @product = Product.where(slug: params[:slug]).includes(:category).first
     @images = ProductImage.where(product_id: @product)
+    @technical_details = TechnicalDetail.where(product_id: @product)
     @order_item = current_order.order_items.new
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -258,35 +258,43 @@ categories_data = [
 technical_details_data = [
   {
     title: "Composition",
-    detail: "Liège expansé pur 100 %"
+    detail: "Liège expansé pur 100 %",
+    product_id: 1
   },
   {
     title: "Provenance",
-    detail: "Issu du chêne liège, récolté tous les septs ans. Produit non renouvelable."
+    detail: "Issu du chêne liège, récolté tous les septs ans. Produit non renouvelable.",
+    product_id: 1
   },
   {
     title: "Dimensions",
-    detail: "1 m x 0,50 m<br>Existe en épaisseurs : 10-20-30-40-50-60-80-100-120-150 "
+    detail: "1 m x 0,50 m<br>Existe en épaisseurs : 10-20-30-40-50-60-80-100-120-150 ",
+    product_id: 1
   },
   {
     title: "Conductivité thermique",
-    detail: "0,037 à 0,04 w / m°C"
+    detail: "0,037 à 0,04 w / m°C",
+    product_id: 1
   },
   {
     title: "Capacité thermique",
-    detail: "1670 J / kg°K"
+    detail: "1670 J / kg°K",
+    product_id: 1
   },
   {
     title: "Densité",
-    detail: "105 kg / m3"
+    detail: "105 kg / m3",
+    product_id: 1
   },
   {
     title: "Classement au feu",
-    detail: "E"
+    detail: "E",
+    product_id: 1
   },
   {
     title: "Affaiblissement accoustique",
-    detail: "Jusqu’à 34 dB selon la mise en oeuvre"
+    detail: "Jusqu’à 34 dB selon la mise en oeuvre",
+    product_id: 1
   }
 ]
 
@@ -307,8 +315,5 @@ image_product = Rails.root.join("app/assets/images/image_produit.png").open
 if Product.count == 0
   product = Product.create! id: 1, name: "Rouleau de liège naturel", price: 5.49, active: true, category_id: 1, slug: "rouleau-de-liege-naturel", marque: "ISOCOR", description: "Liège naturel expansé pur conditionné en panneau rigide. Isolant thermique et phonique, le liège est obtenu à partir du chêne liège, réduit en granulés puis expansé à la vapeur. L'énergie, nécessaire à l'expansion du liège est issue à 95% de la biomasse et 5 % de l’électricité. Particulièrement adapté au milieu humide, le liège est imputrescible, résistant à l'écrasement, absorbant acoustique et auto extinguible. Durable, le liège conserve ses caractéristiques longtemps et il n'est pas attaqué par les insectes."
   ProductImage.create! id: 1, product: product, image: image_product, order: 0
-  technical_details_data.each do |technical_detail|
-    technical_detail = technical_detail.to_hash
-    TechnicalDetail.create!(title: technical_detail['title'], detail: technical_detail['detail'], product: product)
-  end
+  TechnicalDetail.create!(technical_details_data)
 end
