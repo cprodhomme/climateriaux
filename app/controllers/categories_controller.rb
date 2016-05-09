03544a1a8b6a8094b354a19dd2ad1d6abf95a7bb
Cle61 <prodhomme.clement@hotmail.fr>
2016-05-09 14:29:33 +0200

    intégration de la page category

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -4,8 +4,8 @@ class CategoriesController < ApplicationController
   end
 
   def show
-    category = Category.where(slug: params[:slug])
-    @products = Product.where(category_id: category)
+    @category = Category.where(slug: params[:slug]).first
+    @products = Product.where(category_id: @category)
     @order_item = current_order.order_items.new
   end
 
