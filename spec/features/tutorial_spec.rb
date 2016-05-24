f00651e5f0422363c82c2c3158834307056eaa2e
Clément Prod'homme <prodhomme.clement@hotmail.fr>
2016-05-25 00:37:04 +0200

    Update tutorial_spec.rb

--- spec/features/tutorial_spec.rb
+++ spec/features/tutorial_spec.rb
@@ -4,12 +4,12 @@ describe 'Tutorial', type: :feature do
   scenario "j'accède à la page index des tutoriels" do
   	tutorial = create :tutorial
     visit tutorials_path
-    expect(page).to have_content "Titre Tutoriel"
+    expect(page).to have_content "Titre tutoriel"
   end
 
   scenario "j'accède à la page d'un tutoriel" do
   	tutorial = create :tutorial, slug: "tuto"
     visit tutorial_path(slug: "tuto")
-    expect(page).to have_content "Titre Tutoriel"
+    expect(page).to have_content "Titre tutoriel"
   end
 end
