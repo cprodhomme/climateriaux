403519c93de17b158e2493974a506dd22c95da08
Cle61 <prodhomme.clement@hotmail.fr>
2016-05-25 10:57:18 +0200

    fix test pour les tutoriels

--- spec/features/tutorial_spec.rb
+++ spec/features/tutorial_spec.rb
@@ -2,13 +2,13 @@ require 'rails_helper'
 
 describe 'Tutorial', type: :feature do
   scenario "j'accède à la page index des tutoriels" do
-  	tutorial = create :tutorial
+  	tutorial = create :tutorial, title: "Titre tutoriel"
     visit tutorials_path
     expect(page).to have_content "Titre tutoriel"
   end
 
   scenario "j'accède à la page d'un tutoriel" do
-  	tutorial = create :tutorial, slug: "tuto"
+  	tutorial = create :tutorial, title: "Titre tutoriel", slug: "tuto"
     visit tutorial_path(slug: "tuto")
     expect(page).to have_content "Titre tutoriel"
   end
