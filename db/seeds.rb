01a87a3ca01175e9047f5f54fe3b03cfaee080ef
Cle61 <prodhomme.clement@hotmail.fr>
2016-05-25 14:20:21 +0200

    refactoring du seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,49 +3,6 @@
 
 require 'json'
 
-technical_details_data = [
-  {
-    title: "Composition",
-    detail: "Liège expansé pur 100 %",
-    product_id: 1
-  },
-  {
-    title: "Provenance",
-    detail: "Issu du chêne liège, récolté tous les septs ans. Produit non renouvelable.",
-    product_id: 1
-  },
-  {
-    title: "Dimensions",
-    detail: "1 m x 0,50 m<br>Existe en épaisseurs : 10-20-30-40-50-60-80-100-120-150 ",
-    product_id: 1
-  },
-  {
-    title: "Conductivité thermique",
-    detail: "0,037 à 0,04 w / m°C",
-    product_id: 1
-  },
-  {
-    title: "Capacité thermique",
-    detail: "1670 J / kg°K",
-    product_id: 1
-  },
-  {
-    title: "Densité",
-    detail: "105 kg / m3",
-    product_id: 1
-  },
-  {
-    title: "Classement au feu",
-    detail: "E",
-    product_id: 1
-  },
-  {
-    title: "Affaiblissement accoustique",
-    detail: "Jusqu’à 34 dB selon la mise en oeuvre",
-    product_id: 1
-  }
-]
-
 if Category.count == 0
   categories = []
   categoriesJSON = ActiveSupport::JSON.decode(File.read('db/seeds/categories.json'))
@@ -70,7 +27,11 @@ image_product = Rails.root.join("app/assets/images/image_produit.png").open
 if Product.count == 0
   product = Product.create! id: 1, name: "Rouleau de liège naturel", price: 5.49, active: true, category_id: 1, slug: "rouleau-de-liege-naturel", marque: "ISOCOR", description: "Liège naturel expansé pur conditionné en panneau rigide. Isolant thermique et phonique, le liège est obtenu à partir du chêne liège, réduit en granulés puis expansé à la vapeur. L'énergie, nécessaire à l'expansion du liège est issue à 95% de la biomasse et 5 % de l’électricité. Particulièrement adapté au milieu humide, le liège est imputrescible, résistant à l'écrasement, absorbant acoustique et auto extinguible. Durable, le liège conserve ses caractéristiques longtemps et il n'est pas attaqué par les insectes."
   ProductImage.create! id: 1, product: product, image: image_product, order: 0
-  TechnicalDetail.create!(technical_details_data)
+  
+  technical_detailsJSON = ActiveSupport::JSON.decode(File.read('db/seeds/technical_details.json'))
+  technical_detailsJSON.each do |td|
+    TechnicalDetail.create!(title: td['title'], detail: td['detail'], product_id: td['product_id'])
+  end
 
   logo_merchant = Rails.root.join("app/assets/images/logo_merchant.png").open
   merchant = Merchant.create! id: 1, name: "Smartagro", description: "Je suis un marchant de test", logo: logo_merchant
