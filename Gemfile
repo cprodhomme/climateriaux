3131cfdeecfc6cd8ec2f12a8318cf327f6cd07cf
Cle61 <prodhomme.clement@hotmail.fr>
2016-05-27 14:33:55 +0200

    ajout de la gem cancancan pour gerer les permissions des utilisateurs

--- Gemfile
+++ Gemfile
@@ -33,6 +33,8 @@ gem 'binding_of_caller'
 
 gem 'kaminari'
 
+gem 'cancancan'
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160523213703) do
+ActiveRecord::Schema.define(version: 20160527123142) do
 
   create_table "categories", force: :cascade do |t|
     t.string   "name",        limit: 255
@@ -131,6 +131,7 @@ ActiveRecord::Schema.define(version: 20160523213703) do
     t.string   "last_sign_in_ip",        limit: 255
     t.datetime "created_at",                                      null: false
     t.datetime "updated_at",                                      null: false
+    t.string   "role",                   limit: 255
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
