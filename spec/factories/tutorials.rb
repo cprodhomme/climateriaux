7b75a6e36f61ceca7f568cf3de8ca9ae6ee125e6
Cle61 <prodhomme.clement@hotmail.fr>
2016-05-28 10:54:55 +0200

    ameliore les tests

--- spec/factories/tutorials.rb
+++ spec/factories/tutorials.rb
@@ -1,6 +1,7 @@
 FactoryGirl.define do
   factory :tutorial do
   	title "titre de mon tuto"
+    resume "lorem ipsum"
     content "contenu de mon tuto"
     slug "tuto"
   end
--- spec/features/home_spec.rb
+++ spec/features/home_spec.rb
@@ -2,7 +2,8 @@ require 'rails_helper'
 
 describe 'Home', type: :feature do
   scenario "j'accède à la page home" do
+    tutorial = create :tutorial, title: "Titre tutoriel"
     visit root_path
-    expect(page).to have_http_status(200)
+    expect(page).to have_content "Titre tutoriel"
   end
 end
