82f298429d08895c91ebb81fa512def50111398b
Cle61 <prodhomme.clement@hotmail.fr>
2016-05-28 10:49:41 +0200

    correction des validations du tutoriel

--- app/models/tutorial.rb
+++ app/models/tutorial.rb
@@ -2,5 +2,5 @@ class Tutorial < ActiveRecord::Base
   belongs_to :user
   mount_uploader :image, ImageUploader
 
-  validates :name, :resume, :content, presence: true
+  validates :title, :resume, :content, presence: true
 end
