21295a9976a192a43953f654f436d1420b237d51
Cle61 <prodhomme.clement@hotmail.fr>
2016-05-29 17:53:01 +0200

    modifie la preconpilation des assets

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,59 @@ module Climateriaux
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    # setup bower components folder for lookup
+    config.assets.paths << Rails.root.join('vendor', 'assets', 'bower_components')
+    # fonts
+    config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
+    # images
+    config.assets.precompile << /\.(?:png|jpg)$/
+    # precompile vendor assets
+    config.assets.precompile += %w( base.js )
+    config.assets.precompile += %w( base.css )
+    # precompile themes
+    config.assets.precompile += ['admin/angle/themes/theme-a.css',
+                                 'admin/angle/themes/theme-b.css',
+                                 'admin/angle/themes/theme-c.css',
+                                 'admin/angle/themes/theme-d.css',
+                                 'admin/angle/themes/theme-e.css',
+                                 'admin/angle/themes/theme-f.css',
+                                 'admin/angle/themes/theme-g.css',
+                                 'admin/angle/themes/theme-h.css'
+                                ]
+    # Controller assets
+    config.assets.precompile += [
+                                 # Scripts
+                                 'charts.js',
+                                 'dashboard.js',
+                                 'documentation.js',
+                                 'elements.js',
+                                 'extras.js',
+                                 'forms.js',
+                                 'maps.js',
+                                 'multilevel.js',
+                                 'pages.js',
+                                 'tables.js',
+                                 'widgets.js',
+                                 'blog.js',
+                                 'ecommerce.js',
+                                 'forum.js',
+                                 # Stylesheets
+                                 'charts.css',
+                                 'dashboard.css',
+                                 'documentation.css',
+                                 'elements.css',
+                                 'extras.css',
+                                 'forms.css',
+                                 'maps.css',
+                                 'multilevel.css',
+                                 'pages.css',
+                                 'tables.css',
+                                 'widgets.css',
+                                 'blog.css',
+                                 'ecommerce.css',
+                                 'forum.css'
+                                ]
   end
 end
+
