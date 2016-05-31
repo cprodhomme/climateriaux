0be0951cd7b67a95a8f2659318d35d7376ea4998
Cle61 <prodhomme.clement@hotmail.fr>
2016-05-31 18:59:41 +0200

    permet d'editer un utilisateur ou supprimer

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -27,19 +27,38 @@ class Admin::UsersController < Admin::DashboardController
   def update
     @user = User.find(params[:id])
 
-    if @user.update_attributes(user_params)
-      redirect_to edit_admin_user_path(@user.id), notice: 'Utilisateur à bien été modifié'
+    if needs_password?(@user, user_params)
+      if @user.update_attributes(user_params)
+        redirect_to edit_admin_user_path(@user.id), notice: 'Utilisateur a bien été modifié'
+      else
+        render 'edit'
+      end
     else
-      render 'edit'
+      if @user.update_attributes(user_params_without_password)
+        redirect_to edit_admin_user_path(@user.id), notice: 'Utilisateur a bien été modifié'
+      else
+        render 'edit'
+      end
     end
   end
 
-  def delete
+  def destroy
+    User.destroy(params[:id])
+    redirect_to admin_users_path, notice: "L'utisateur a bien été supprimé"
   end
 
   private
 
+  def needs_password?(user, user_params)
+    !user_params[:password].blank?
+  end
+
   def user_params
+    params.require(:user).permit(:email, :password,
+                                 :password_confirmation, :role)
+  end
+
+  def user_params_without_password
     params.require(:user).permit(:email, :password,
                                  :password_confirmation, :role)
   end
