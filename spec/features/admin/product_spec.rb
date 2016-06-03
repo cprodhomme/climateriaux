9fa83c330ee8f5acb981b24490ea8159ac27ae81
Cle61 <prodhomme.clement@hotmail.fr>
2016-06-03 19:51:58 +0200

    ajout d'un test sur la suppression d'un produit et inte de la page edit d'un produit

--- spec/features/admin/product_spec.rb
+++ spec/features/admin/product_spec.rb
@@ -10,11 +10,18 @@ describe 'admin/product', type: :feature do
     end
 
     scenario "je peux voir tous les produits" do
-      produit_1 = create :product, name: "produit 1"
-      produit_2 = create :product, name: "produit 2"
+      product_1 = create :product, name: "produit 1"
+      product_2 = create :product, name: "produit 2"
       visit admin_products_path
       expect(page).to have_content "produit 1"
       expect(page).to have_content "produit 2"
+    end
+
+    scenario "je peux supprimer un produit" do
+      product_1 = create :product, name: "produit 1"
+      visit admin_products_path
+      page.find("#delete-product-#{product_1.id}").click
+      expect(page).to_not have_content "produit 1"
     end
   end
 end
