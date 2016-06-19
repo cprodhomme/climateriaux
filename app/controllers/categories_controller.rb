3e2a1f7f36266e436d8f46f4f8e01f5448c51dcf
Cle61 <prodhomme.clement@hotmail.fr>
2016-06-19 21:20:49 +0200

    correction du fil d'ariane

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -6,6 +6,9 @@ class CategoriesController < ApplicationController
   def show
     @category = Category.where(slug: params[:slug]).first
     @products = Category.all_product @category
+    if @category.parent_id
+      @parent_category = Category.where(id: @category.parent_id).take!
+    end
   end
 
 end
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,5 +2,8 @@ class ProductsController < ApplicationController
   def show
     @product = Product.where(slug: params[:slug]).first
     @order_item = current_order.order_items.new
+    if @product.category.parent_id
+      @parent_category = Category.where(id: @product.category.parent_id).take!
+    end
   end
 end
