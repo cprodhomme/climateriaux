518c3247c64ca8ece6ff82d11814b211bb1f397b
Cle61 <prodhomme.clement@hotmail.fr>
2016-06-19 20:04:33 +0200

    affiche les images des produits du la page category show

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -5,8 +5,7 @@ class CategoriesController < ApplicationController
 
   def show
     @category = Category.where(slug: params[:slug]).first
-    @products = Category.all_product @category 
-    @order_item = current_order.order_items.new
+    @products = Category.all_product @category
   end
 
 end
