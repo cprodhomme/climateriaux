fe8929321e1bc28c42fa002c29b302f823f809e1
Cle61 <prodhomme.clement@hotmail.fr>
2016-06-20 23:02:12 +0200

    met a jour le seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -25,7 +25,8 @@ end
 image_product = Rails.root.join("app/assets/images/image_produit.png").open
 
 if Product.count == 0
-  product = Product.create! id: 1, name: "Rouleau de liège naturel", price: 5.49, active: true, category_id: 1, slug: "rouleau-de-liege-naturel", marque: "ISOCOR", description: "Liège naturel expansé pur conditionné en panneau rigide. Isolant thermique et phonique, le liège est obtenu à partir du chêne liège, réduit en granulés puis expansé à la vapeur. L'énergie, nécessaire à l'expansion du liège est issue à 95% de la biomasse et 5 % de l’électricité. Particulièrement adapté au milieu humide, le liège est imputrescible, résistant à l'écrasement, absorbant acoustique et auto extinguible. Durable, le liège conserve ses caractéristiques longtemps et il n'est pas attaqué par les insectes."
+  merchant = Merchant.create! id: 1, name: "Smartagro", description: "Je suis un marchant de test", logo: logo_merchant
+  product = Product.create! id: 1, name: "Rouleau de liège naturel", price: 5.49, active: true, category_id: 1, merchant_id: 1, quantity: 10, slug: "rouleau-de-liege-naturel", marque: "ISOCOR", description: "Liège naturel expansé pur conditionné en panneau rigide. Isolant thermique et phonique, le liège est obtenu à partir du chêne liège, réduit en granulés puis expansé à la vapeur. L'énergie, nécessaire à l'expansion du liège est issue à 95% de la biomasse et 5 % de l’électricité. Particulièrement adapté au milieu humide, le liège est imputrescible, résistant à l'écrasement, absorbant acoustique et auto extinguible. Durable, le liège conserve ses caractéristiques longtemps et il n'est pas attaqué par les insectes."
   ProductImage.create! id: 1, product: product, image: image_product, order: 0
   
   technical_detailsJSON = ActiveSupport::JSON.decode(File.read('db/seeds/technical_details.json'))
@@ -34,7 +35,6 @@ if Product.count == 0
   end
 
   logo_merchant = Rails.root.join("app/assets/images/logo_merchant.png").open
-  merchant = Merchant.create! id: 1, name: "Smartagro", description: "Je suis un marchant de test", logo: logo_merchant
   ProductMerchant.create! id: 1, product: product, merchant: merchant, stock: 3
 end
 
